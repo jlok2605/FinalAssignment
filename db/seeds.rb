70c1fc7239b45ee335ac7c946ba9826a2d451692
JeremyLok <jeremylok3@gmail.com>
2024-01-10 09:06:43 +1000

    making faker books

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Book.create(title:Faker::Book.title,author:Faker::Book.author,genre:Faker::Book.genre,yearpublished:rand(2024))
