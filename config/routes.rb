89ca4dc2eb4bc13bd306651725bdbf500844767a
jlok2605 <jeremylok3@gmail.com>
2024-02-01 21:58:06 +1000

    in process of linking views to signup page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   end
   root "books#index"
   get '/user', to: 'users#index'
+  get '/user/sign_up', to: 'users#new', as: 'new_user_registration'
+  get '/user/sign_up', to: 'users#create' , as: 'user_registration'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
