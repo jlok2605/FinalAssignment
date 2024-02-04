578fcb4329f5946d03bd6dbd6031f6a0fd6d606d
jlok2605 <jeremylok3@gmail.com>
2024-02-04 11:55:28 +1000

    working adding users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,41 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-    end
-    def create
-      @user = User.new(user_params)
-  
-      if (@user.password == @user.password_confirmation) && @user.save
-        # session[:user_id] = @user.id
-        # User created successfully
-        if @user.admin? && @user.authenticate(params[:user][:admin_code])
-          # Admin-specific logic
-          session[:admin_user_id] = @user.id
-          redirect_to root_path, notice: "Admin signup successful"
-        else
-          # Regular user signup successful
-          redirect_to root_path, notice: "User signup successful"
-        end
-      else
-        # Password and confirmation don't match, or save failed
-        flash.now[:alert] = 'Passwords do not match. Please ensure they match'
-        puts("Password do not match!!")
-      end
-    end
-  
-    def index
-      @users = User.all
-      render json: @users, status: :ok
-    end
-      def show
-      @user = User.find(params[:id]) 
-      render json: @user, status: :ok
-    end
+  def new
+    @user = User.new
+  end
 
-    def user_params
-      params.require(:user).permit(:username, :password,  :password_confirmation)
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+
+      if @user.admin? && @user.authenticate(params[:user][:admin_code])
+        session[:admin_user_id] = @user.id
+        flash.now[:notice] = 'Admin signup successful'
+      else
+        flash.now[:notice] = 'User signup successful'
+      end
+
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'Failed to create user. Please check the form for errors.'
+      render :new
     end
+  end
+
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
+  def show
+    @user = User.find(params[:id]) 
+    render json: @user, status: :ok
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
