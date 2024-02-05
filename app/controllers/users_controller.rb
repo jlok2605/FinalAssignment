41d4719f614406baceee7893e5db4555249af21c
jlok2605 <jeremylok3@gmail.com>
2024-02-05 16:40:14 +1000

    testing to make users working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,81 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      session[:user_id] = @user.id
-
-      if @user.admin? && @user.authenticate(params[:user][:admin_code])
-        session[:admin_user_id] = @user.id
-        flash.now[:notice] = 'Admin signup successful'
-      else
-        flash.now[:notice] = 'User signup successful'
-      end
-
-      redirect_to root_path
-    else
-      flash.now[:alert] = 'Failed to create user. Please check the form for errors.'
-      render :new
+    def new
+        @user = User.new
     end
-  end
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to user_path(@user),notice: 'USer created successfully'
+      else
+        redirect_to root_path
+      end
+    end
+  
+      # if (@user.password == @user.password_confirmation) && @user.save
+      #   # User created successfully
+      #   if @user.admin? && @user.authenticate(params[:user][:admin_code])
+      #     # Admin-specific logic
+      #     session[:admin_user_id] = @user.id
+      #     redirect_to root_path, notice: "Admin signup successful"
+      #   else
+      #     # Regular user signup successful
+      #     redirect_to root_path, notice: "User signup successful"
+      #   end
+      # else
+      #   # Password and confirmation don't match, or save failed
+      #   flash.now[:alert] = 'Passwords do not match. Please ensure they match'
+      #     puts 'Not working'
+      # end
+  
+    def index
+      @users = User.all
+      render json: @users, status: :ok
+    end
+      def show
+      @user = User.find(params[:id]) 
+      render json: @user, status: :ok
+    end
 
-  def index
-    @users = User.all
-    render json: @users, status: :ok
-  end
+    def user_params
+      params.require(:user).permit(:username, :password,  :password_confirmation)
+    end
+  # def new
+  #   @user = User.new
+  # end
 
-  def show
-    @user = User.find(params[:id]) 
-    render json: @user, status: :ok
-  end
+  # def create
+  #   @user = User.new(user_params)
 
-  private
+  #   if @user.save
+  #     session[:user_id] = @user.id
 
-  def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
-  end
+  #     if @user.admin? && @user.authenticate(params[:user][:admin_code])
+  #       session[:admin_user_id] = @user.id
+  #       flash.now[:notice] = 'Admin signup successful'
+  #     else
+  #       flash.now[:notice] = 'User signup successful'
+  #     end
+
+  #     redirect_to root_path
+  #   else
+  #     flash.now[:alert] = 'Failed to create user. Please check the form for errors.'
+  #     render :new
+  #   end
+  # end
+
+  # def index
+  #   @users = User.all
+  #   render json: @users, status: :ok
+  # end
+
+  # def show
+  #   @user = User.find(params[:id]) 
+  #   render json: @user, status: :ok
+  # end
+
+  # private
+
+  # def user_params
+  #   params.require(:user).permit(:username, :password, :password_confirmation)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,33 @@
-# Rails.application.routes.draw do
 
+Rails.application.routes.draw do
+  resources :books, only: [:index, :show, :create, :destroy, :new]
+  resources :users, only: [:new, :create, :index, :show]
+  resources :borrowed_books, only: [:create, :destroy]
+  resources :users do
+    resources :borrowed_books, only: [:index]
+  end
+  root "books#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+end
+
+# Rails.application.routes.draw do
 #   resources :books, only: [:index, :show, :create, :destroy, :new]
-#   resources :users, only: [:new, :create, :index, :show]
-#   resources :borrowed_books, only: [:index, :create, :destroy]
-#   resources :users do
-#     resources :borrowed_books, only: [] do
+#   resources :users, only: [:new, :create, :index, :show] do 
+#     resources :borrowed_books, only: [ ] do
 #       post 'borrow_book', on: :member
 #     end
 #   end
+
+
 #   root "books#index"
-#   get '/user', to: 'users#index'
-#   get '/user/sign_up', to: 'users#new', as: 'new_user_registration'
-#   get '/user/sign_up', to: 'users#create' , as: 'user_registration'
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 #   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
 #   # Can be used by load balancers and uptime monitors to verify that the app is live.
 #   get "up" => "rails/health#show", as: :rails_health_check
 # end
-Rails.application.routes.draw do
-  resources :books, only: [:index, :show, :create, :destroy, :new]
-  resources :users, only: [:new, :create, :index, :show] do 
-    resources :borrowed_books, only: [ ] do
-      post 'borrow_book', on: :member
-    end
-  end
-
-
-  root "books#index"
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-end
 
