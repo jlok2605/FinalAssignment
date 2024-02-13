347728c3f79a9ccee629db1ebd8442b4c520cf8f
jlok2605 <jeremylok3@gmail.com>
2024-02-13 16:12:22 +1000

    qty working correctly for check if 0

--- app/controllers/borrowed_books_controller.rb
+++ app/controllers/borrowed_books_controller.rb
@@ -4,10 +4,13 @@ class BorrowedBooksController < ApplicationController
     def create
       borrowed_book = BorrowedBook.create(borrowed_book_params)
       book = Book.find(borrowed_book.book_id)
-      book.update(quantity: book.quantity - 1)
-      render json: { message: 'Borrowed book' }, status: :created
+      if book.quantity = 0
+        render json: {error: "No copies of this book are available"}, status: :not_found
+      else
+        book.update(quantity: book.quantity - 1)
+        render json: { message: 'Borrowed book' }, status: :created
+      end
     end
-  
     private  
     def borrowed_book_params
       params.permit(:user_id, :book_id)
