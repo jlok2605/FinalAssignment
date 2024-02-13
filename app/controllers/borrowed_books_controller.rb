1593decc11d144bad6ac34f2327e4d4a6f1130c4
jlok2605 <jeremylok3@gmail.com>
2024-02-13 17:12:53 +1000

    working borrowing and checkout books

--- app/controllers/borrowed_books_controller.rb
+++ app/controllers/borrowed_books_controller.rb
@@ -1,19 +1,34 @@
 
     # before_action :authenticate_user!, only: [:create, :return]
 class BorrowedBooksController < ApplicationController
+    #To borrow a book
     def create
       borrowed_book = BorrowedBook.create(borrowed_book_params)
       book = Book.find(borrowed_book.book_id)
-      if book.quantity = 0
-        render json: {error: "No copies of this book are available"}, status: :not_found
-      else
+      if book.quantity == 0
+        render json: {error: "No copies of this book are available"}, status: :unprocessable_entity
+      return
+      end
         book.update(quantity: book.quantity - 1)
         render json: { message: 'Borrowed book' }, status: :created
-      end
+    end
+    #TO RETURN A BOOK
+    def destroy
+        borrowed_book = BorrowedBook.find(params[:id])
+        book = Book.find(borrowed_book.book_id)
+        book.update(quantity: book.quantity + 1)
+        borrowed_book.destroy 
+        render json: {message: "Book returned"}, status: :ok
+    end
+    def index
+        borrowed_books = BorrowedBook.all
+        render json: borrowed_books, status: :ok
     end
     private  
     def borrowed_book_params
       params.permit(:user_id, :book_id)
     end
+
+   
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   resources :books, only: [:index, :show, :create, :destroy]
   resources :users, only: [:new, :create, :index, :show]
-  resources :borrowed_books, only: [:create]
+  resources :borrowed_books, only: [:create, :destroy, :index]
   resources :users do
     resources :borrowed_books, only: [:index]
   end
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   delete '/logout', to: 'sessions#destroy'
   get '/me', to: "users#show" 
-  post '/borrowed_books', to: 'create#borrow_book'
+  post '/borrowed_books', to: 'borrowed_books#create'
+  delete '/borrowed_books', to: 'borrowed_books#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
