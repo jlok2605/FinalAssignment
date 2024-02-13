08137869b189c2af050840926bbd2daad93e27d8
jlok2605 <jeremylok3@gmail.com>
2024-02-13 16:08:34 +1000

    borrowing books now updates quantity

--- app/controllers/borrowed_books_controller.rb
+++ app/controllers/borrowed_books_controller.rb
@@ -3,11 +3,12 @@
 class BorrowedBooksController < ApplicationController
     def create
       borrowed_book = BorrowedBook.create(borrowed_book_params)
+      book = Book.find(borrowed_book.book_id)
+      book.update(quantity: book.quantity - 1)
       render json: { message: 'Borrowed book' }, status: :created
     end
   
-    private
-  
+    private  
     def borrowed_book_params
       params.permit(:user_id, :book_id)
     end
--- app/models/borrowed_book.rb
+++ app/models/borrowed_book.rb
@@ -2,4 +2,7 @@ class BorrowedBook < ApplicationRecord
     belongs_to :user
     belongs_to :book
 
+    def book_quantity
+        book.quantity
+    end
 end
