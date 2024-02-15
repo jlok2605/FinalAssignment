87a5a58f43ff60a8fe2cdd79ff048d0e78d6e1b9
jlok2605 <jeremylok3@gmail.com>
2024-02-15 12:13:18 +1000

    trying to fix the all books page

--- client/src/bookinfo.js
+++ client/src/bookinfo.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import BorrowButton from "./borrowbutton"
-
+import "./bookcontainer.css"
 
 function BookInfo(props){
     const book = props.book
 
 
     return (
-        <div className="container">
-            <img src = "../bookcover.jpg" alt = "book"></img>
-            <div className="information">
-                <h3 key = "title">Title: {book.title}</h3>
-                <h3 key = "author">Author: {book.author}</h3>
-                <h3 key = "genre">Genre: {book.genre}</h3>
-                <h3 key = "yearpublished">Year Published: {book.yearpublished}</h3>
+        <div id="books">
+            <img src = "../bookcover.jpg" alt = "book" id="bookcover"></img>
+            <div className="bookinformation">
+                <h5 key = "title">Title: {book.title}</h5>
+                <h5 key = "author">Author: {book.author}</h5>
+                <h5 key = "genre">Genre: {book.genre}</h5>
+                <h5 key = "yearpublished">Year Published: {book.yearpublished}</h5>
                 <BorrowButton bookId={book.id}/>
             </div>
         </div>
